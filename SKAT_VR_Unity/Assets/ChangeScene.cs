2ee433045f5650d29e30dca1a8a122cd632638d7
Mixchr <mikkelchristiansen@Innovation3s-MacBook-Pro.local>
2017-05-16 13:28:59 +0200

    New delay

---
+++ SKAT_VR_Unity/Assets/ChangeScene.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour
+
+{
+
+	void Start()
+	{
+		
+	}
+
+	void Update ()
+	{
+		
+	}
+	IEnumerator Example()
+	{
+		
+	yield return new WaitForSeconds(5);
+	// Only specifying the sceneName or sceneBuildIndex will load the scene with the Single mode
+	SceneManager.LoadScene("Lydrejse", LoadSceneMode.Single);
+
+	}
+
+}
