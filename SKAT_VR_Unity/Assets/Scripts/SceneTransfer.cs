23743ff001ed910db371af50ccd10f7e377de9c7
Oragada <Oragada@gmail.com>
2017-05-16 15:48:18 +0200

    SceneTransfer update

--- SKAT_VR_Unity/Assets/Scripts/SceneTransfer.cs
+++ SKAT_VR_Unity/Assets/Scripts/SceneTransfer.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SceneTransferMovie : MonoBehaviour {
+public class SceneTransfer : MonoBehaviour {
 
     public Renderer screen;
 
