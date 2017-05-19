2058261a231cbb3634a733ad70c4c82bd5211269
Oragada <Oragada@gmail.com>
2017-05-19 13:13:36 +0200

    minor edit

--- SKAT_VR_Unity/Assets/Script/Controller/RadioController.cs
+++ SKAT_VR_Unity/Assets/Script/Controller/RadioController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class RadioController : MonoBehaviour {
 
-	public GameObject[] sounds = new GameObject[5];
+	public GameObject[] sounds;
 
 	// Use this for initialization
 	void Start () {
